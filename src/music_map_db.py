5d5cbe154b9cdb398397f14418e43cb14634bc86
Justin Levine <justin.c.levine@gmail.com>
2012-05-06 11:57:43 -0400

    Finished first pass DB design. Gunna try it out.

--- src/music_map_db.py
+++ src/music_map_db.py
@@ -6,8 +6,11 @@ if __name__ == "__main__":
 
     c = db.cursor()
 
+    #===========================================================================
+    # SONG TABLE
     # A table of all the unique individual songs
     # (with standardized sanitized names) that you know about
+    #===========================================================================
     c.execute("""CREATE TABLE song (song_id     INTEGER       PRIMARY KEY
                                   , artist_key  VARCHAR2(200) NOT NULL    -- The unique key
                                   , album_key   VARCHAR2(500) NOT NULL
@@ -23,7 +26,10 @@ if __name__ == "__main__":
                                                 , title_key)
               """)
 
-    # Create music_map table
+    #===========================================================================
+    # MUSIC MAP TABLE
+    # A table that maps a song to what it actually looks like in a particular location
+    #===========================================================================
     c.execute("""CREATE TABLE music_map (music_map_id INTEGER       PRIMARY KEY
                                        , song_id      INTEGER
                                        , location     VARCHAR2(100) NOT NULL    -- Root location of all the songs.
@@ -36,25 +42,68 @@ if __name__ == "__main__":
 
     # Only one song in each location
     c.execute("""ALTER TABLE music_map
-             ADD CONSTRAINTS u_loc UNIQUE (artist_key
-                                         , album_key
-                                         , track_key
-                                         , title_key
-                                         , location""")
+             ADD CONSTRAINTS u_loc UNIQUE (song_id
+                                         , location
+                                         , artist
+                                         , album
+                                         , track
+                                         , title)
+              """)
 
     # Index for finding a particular song in a location. Often used to see
     # if a particular song is already inserted for a location.
-    c.execute("""CREATE INDEX music_map_key_idx
-                           ON music_map (artist_key
-                                       , album_key
-                                       , track_key
-                                       , title_key
-                                       , location)""")
+    c.execute("""CREATE INDEX song_loc_idx
+                           ON music_map (song_id
+                                       , location)
+              """)
 
     # Index for deleting all songs for a location
     c.execute("""CREATE INDEX location_idx
-                           ON music_map (location)""")
+                           ON music_map (location)
+              """)
 
-    # Create tags table
+    #===========================================================================
+    # TAG TABLE
+    # Maps a song to a tag
+    #===========================================================================
     c.execute("""CREATE TABLE music_tag (music_tag_id INTEGER PRIMARY KEY
-                                       , music_map_id)""")
+                                       , song_id      INTEGER
+                                       , tag          VARCHAR2(50)
+                                       , FOREIGN KEY(song_id) REFERENCES song(song_id))
+              """)
+
+    # Only only one row per tag-song. However, a song can have many tags.
+    c.execute("""ALTER TABLE music_tag
+             ADD CONSTRAINTS u_tag UNIQUE (song_id
+                                         , tag)
+              """)
+
+    # Index for finding all songs for a tag
+    c.execute("""CREATE INDEX tag_idx
+                           ON music_tag (tag)
+              """)
+
+    # Index for finding all tags for a song
+    c.execute("""CREATE INDEX tag_idx
+                           ON music_tag (song_id)
+              """)
+
+    #===========================================================================
+    # RATINGS TABLE
+    # Maps a song to a rating
+    #===========================================================================
+    c.execute("""CREATE TABLE music_rating (music_rating_id INTEGER PRIMARY KEY
+                                          , song_id         INTEGER
+                                          , rating          INTEGER              -- 1 = never. 2 = sometimes. 3 = always.
+                                          , FOREIGN KEY(song_id) REFERENCES song(song_id))
+              """)
+
+    # Only only one rating per song.
+    c.execute("""ALTER TABLE music_rating
+             ADD CONSTRAINTS u_rating UNIQUE (song_id)
+              """)
+
+    # Index for finding all songs for a rating
+    c.execute("""CREATE INDEX rating_idx
+                           ON music_tag (rating)
+              """)
