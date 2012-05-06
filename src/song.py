2bcdde00bed5ade9e4b8df611c07983a19605b78
Justin Levine <justin.c.levine@gmail.com>
2012-05-06 13:38:21 -0400

    Add original song properties to Song class.

--- src/song.py
+++ src/song.py
@@ -22,10 +22,18 @@ class Song(object):
             self._track = ss('04')
             self._title = ss('Egmont Overture, Op. 84')
         else:
-            self._artist = ss(matches.group(1), remove_the=True, remove_and=True)
-            self._album = ss(matches.group(2))
-            self._track = ss(matches.group(3))
-            self._title = ss(matches.group(4))
+            # TODO: !3 Some utility to grab regex matches to a dictionary?
+            self._orig_artist = matches.group(1)
+            self._artist = ss(self._orig_artist, remove_the=True, remove_and=True)
+
+            self._orig_album = matches.group(2)
+            self._album = ss(self._orig_album)
+
+            self._orig_track = matches.group(3)
+            self._track = ss(self._orig_track)
+
+            self._orig_title = matches.group(4)
+            self._title = ss(self._orig_title)
 
     @property
     def artist(self):
@@ -42,3 +50,30 @@ class Song(object):
     @property
     def title(self):
         return self._title
+
+    @property
+    def orig_artist(self):
+        return self._orig_artist
+
+    @property
+    def orig_album(self):
+        return self._orig_album
+
+    @property
+    def orig_track(self):
+        return self._orig_track
+
+    @property
+    def orig_title(self):
+        return self._orig_title
+
+    def __repr__(self, *args, **kwargs):
+        return ("{artist} - {album} - {track} {title} | {orig_artist} - {orig_album} - {orig_track} {orig_title}"
+                .format(artist=self.artist,
+                        album=self.album,
+                        track=self.track,
+                        title=self.title,
+                        orig_artist=self.artist,
+                        orig_album=self.album,
+                        orig_track=self.track,
+                        orig_title=self.title,
