eacd0b55be3833c6f2cfb5c2a1f67ca64671fd41
Justin Levine <justin.c.levine@gmail.com>
2012-05-12 06:39:49 -0400

    Remove some more special chars in sanitize string.

--- src/util/string_utils.py
+++ src/util/string_utils.py
@@ -13,6 +13,9 @@ def sanitize_string(s, remove_the=False, remove_and=False):
     s = s.replace("-", " ")
     s = s.replace("(", "")
     s = s.replace(")", "")
+    s = s.replace("[", "")
+    s = s.replace("]", "")
+
     if remove_the:
         s = re.sub(THE_WORD_REGEX, "", s)
 
