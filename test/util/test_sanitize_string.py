91f3a93d3390f1e4fa50d3ccce67025ecd42abc8
Justin Levine <justin.c.levine@gmail.com>
2012-05-11 20:48:50 -0400

    Move sanitize_string tests into util test sub-directory.

--- test/util/test_sanitize_string.py
+++ test/util/test_sanitize_string.py
@@ -1,5 +1,5 @@
 import unittest
-from music_map import MusicMap
+from util import string_utils
 
 
 class SanitizeStringTest(unittest.TestCase):
@@ -7,5 +7,5 @@ class SanitizeStringTest(unittest.TestCase):
     def test_sanitize_lower_case(self):
         uppercased_string = "This IS a teST"
         expected_lowercased_string = 'this is a test'
-        actual_string = MusicMap.sanitize_string(uppercased_string)
+        actual_string = string_utils.sanitize_string(uppercased_string)
         self.assertEqual(expected_lowercased_string, actual_string)
