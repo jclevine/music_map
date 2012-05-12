1e1c6cf799c93212bcb6659f75b5e6201f42113b
Justin Levine <justin.c.levine@gmail.com>
2012-05-11 23:27:34 -0400

    Write extensive tests for sanitizing strings and parsing out songs from song paths.

--- test/test_song.py
+++ test/test_song.py
@@ -10,26 +10,41 @@ class SongTest(unittest.TestCase):
         self.assertEqual('christian death', song.artist)
         self.assertEqual('Christian Death', song.orig_artist)
 
-        self.assertEqual('jesus points the bones at you', song.album)
+        self.assertEqual('jesus points bones at you', song.album)
         self.assertEqual('Jesus Points the Bones at You', song.orig_album)
 
         self.assertEqual('01', song.track)
         self.assertEqual('01', song.orig_track)
 
-        self.assertEqual('believers of the unpure', song.title)
+        self.assertEqual('believers of unpure', song.title)
         self.assertEqual('Believers of the Unpure', song.orig_title)
 
     def test_incorrect_track_number(self):
-        song_path = "//music/A Tribe Called Quest/The Low End Theory/03 - A Tribe Called Quest - Rap Promoter.mp3"
+        song_path = "./media/Backup1/Done./A_Tribe_Called_Quest/The Low End Theory/10.Everything_Is_Fair.mp3"
         song = Song(song_path)
         self.assertEqual('a tribe called quest', song.artist)
-        self.assertEqual('A Tribe Called Quest', song.orig_artist)
+        self.assertEqual('A_Tribe_Called_Quest', song.orig_artist)
 
         self.assertEqual('low end theory', song.album)
         self.assertEqual('The Low End Theory', song.orig_album)
 
+        self.assertEqual('10', song.track)
+        self.assertEqual('10', song.orig_track)
+
+        self.assertEqual('everything is fair', song.title)
+        self.assertEqual('Everything_Is_Fair', song.orig_title)
+
+    def test_complicated_specific_path(self):
+        song_path = "./media/Backup1/Ludwig Van Beethoven/Symphony No. 5 in C minor, Op. 67 Egmont Overture, Op.84/03_Ludwig Van Beethoven_Symphony No. 5 in C minor, Op. 67- Allegro Allegro.mp3"
+        song = Song(song_path)
+        self.assertEqual('ludwig van beethoven', song.artist)
+        self.assertEqual('Ludwig Van Beethoven', song.orig_artist)
+
+        self.assertEqual('symphony no 5 in c minor, op 67 egmont overture, op84', song.album)
+        self.assertEqual('Symphony No. 5 in C minor, Op. 67 Egmont Overture, Op.84', song.orig_album)
+
         self.assertEqual('03', song.track)
         self.assertEqual('03', song.orig_track)
 
-        self.assertEqual('rap promoter', song.title)
-        self.assertEqual('Rap Promoter', song.orig_title)
+        self.assertEqual('symphony no 5 in c minor, op 67 allegro allegro', song.title)
+        self.assertEqual('Symphony No. 5 in C minor, Op. 67- Allegro Allegro', song.orig_title)
--- test/util/test_sanitize_string.py
+++ test/util/test_sanitize_string.py
@@ -1,5 +1,5 @@
 import unittest
-from util import string_utils
+from util.string_utils import sanitize_string
 
 
 class SanitizeStringTest(unittest.TestCase):
@@ -10,7 +10,7 @@ class SanitizeStringTest(unittest.TestCase):
     def test_sanitize_lower_case(self):
         uppercased_string = "This IS a teST"
         expected_lowercased_string = 'this is a test'
-        actual_string = string_utils.sanitize_string(uppercased_string)
+        actual_string = sanitize_string(uppercased_string)
         self.assertEqual(expected_lowercased_string, actual_string)
 
     #===========================================================================
@@ -19,31 +19,37 @@ class SanitizeStringTest(unittest.TestCase):
     def test_sanitize_remove_the(self):
         the_string = "This IS the teST"
         expected_no_the_string = 'this is test'
-        actual_string = string_utils.sanitize_string(the_string, remove_the=True)
+        actual_string = sanitize_string(the_string, remove_the=True)
         self.assertEqual(expected_no_the_string, actual_string)
 
     def test_sanitize_remove_the_at_beginning(self):
         the_string = "THE This IS teST"
         expected_no_the_string = 'this is test'
-        actual_string = string_utils.sanitize_string(the_string, remove_the=True)
+        actual_string = sanitize_string(the_string, remove_the=True)
         self.assertEqual(expected_no_the_string, actual_string)
 
     def test_sanitize_remove_the_at_beginning_no_space(self):
         the_string = "THEThis IS teST"
         expected_no_the_string = 'thethis is test'
-        actual_string = string_utils.sanitize_string(the_string, remove_the=True)
+        actual_string = sanitize_string(the_string, remove_the=True)
         self.assertEqual(expected_no_the_string, actual_string)
 
     def test_sanitize_remove_the_at_end(self):
         the_string = "This IS teST the"
         expected_no_the_string = 'this is test'
-        actual_string = string_utils.sanitize_string(the_string, remove_the=True)
+        actual_string = sanitize_string(the_string, remove_the=True)
         self.assertEqual(expected_no_the_string, actual_string)
 
     def test_sanitize_remove_the_at_end_no_space(self):
         the_string = "This IS teSTthe"
         expected_no_the_string = 'this is testthe'
-        actual_string = string_utils.sanitize_string(the_string, remove_the=True)
+        actual_string = sanitize_string(the_string, remove_the=True)
+        self.assertEqual(expected_no_the_string, actual_string)
+
+    def test_sanitize_dont_remove_the_at_beginning_of_word(self):
+        the_string = "The Low End Theory"
+        expected_no_the_string = 'low end theory'
+        actual_string = sanitize_string(the_string, remove_the=True)
         self.assertEqual(expected_no_the_string, actual_string)
 
     #===========================================================================
@@ -52,31 +58,31 @@ class SanitizeStringTest(unittest.TestCase):
     def test_sanitize_remove_and(self):
         and_string = "This IS And teST"
         expected_no_and_string = 'this is test'
-        actual_string = string_utils.sanitize_string(and_string, remove_and=True)
+        actual_string = sanitize_string(and_string, remove_and=True)
         self.assertEqual(expected_no_and_string, actual_string)
 
     def test_sanitize_remove_and_at_beginning(self):
         and_string = "ANd This IS teST"
         expected_no_and_string = 'this is test'
-        actual_string = string_utils.sanitize_string(and_string, remove_and=True)
+        actual_string = sanitize_string(and_string, remove_and=True)
         self.assertEqual(expected_no_and_string, actual_string)
 
     def test_sanitize_remove_and_at_beginning_no_space(self):
         and_string = "andThis IS teST"
         expected_no_and_string = 'andthis is test'
-        actual_string = string_utils.sanitize_string(and_string, remove_and=True)
+        actual_string = sanitize_string(and_string, remove_and=True)
         self.assertEqual(expected_no_and_string, actual_string)
 
     def test_sanitize_remove_and_at_end(self):
         and_string = "This IS teST anD"
         expected_no_and_string = 'this is test'
-        actual_string = string_utils.sanitize_string(and_string, remove_and=True)
+        actual_string = sanitize_string(and_string, remove_and=True)
         self.assertEqual(expected_no_and_string, actual_string)
 
     def test_sanitize_remove_and_at_end_no_space(self):
         and_string = "This IS teSTanD"
         expected_no_and_string = 'this is testand'
-        actual_string = string_utils.sanitize_string(and_string, remove_and=True)
+        actual_string = sanitize_string(and_string, remove_and=True)
         self.assertEqual(expected_no_and_string, actual_string)
 
     #===========================================================================
@@ -85,11 +91,20 @@ class SanitizeStringTest(unittest.TestCase):
     def test_sanitize_remove_and_and_the(self):
         the_and_and_string = "This IS And teST the"
         expected_no_the_or_and_string = 'this is test'
-        actual_string = string_utils.sanitize_string(the_and_and_string, remove_the=True, remove_and=True)
+        actual_string = sanitize_string(the_and_and_string, remove_the=True, remove_and=True)
         self.assertEqual(expected_no_the_or_and_string, actual_string)
 
     def test_sanitize_remove_and_and_the_adjacent(self):
         the_and_and_string = "ANd the This IS teST"
         expected_no_the_or_and_string = 'this is test'
-        actual_string = string_utils.sanitize_string(the_and_and_string, remove_the=True, remove_and=True)
+        actual_string = sanitize_string(the_and_and_string, remove_the=True, remove_and=True)
         self.assertEqual(expected_no_the_or_and_string, actual_string)
+
+    #===========================================================================
+    # Test Sanitizing Strings with Periods
+    #===========================================================================
+    def test_sanitize_string_with_periods(self):
+        string_with_periods = "Symphony No. 5 in C minor, Op. 67 Egmont Overture, Op.84"
+        expected_string = 'symphony no 5 in c minor, op 67 egmont overture, op84'
+        actual_string = sanitize_string(string_with_periods, remove_the=True, remove_and=True)
+        self.assertEqual(expected_string, actual_string)
