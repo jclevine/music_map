215a67acf40a8e66f54ac0aad7dd81a63cecce7d
Justin Levine <justin.c.levine@gmail.com>
2012-08-11 15:33:18 -0400

    Add some more tests for some scenarios with underscores in the artist name.

--- test/test_music_map.py
+++ test/test_music_map.py
@@ -42,5 +42,47 @@ class TestMusicMap(unittest.TestCase):
             db_data['conn'].close()
 
 
+    def test_underscore_in_artist_name_but_not_in_song(self):
+        db_data = test_utils.insert_playlist_into_music_map('test_artist_has_underscore_but_not_song.m3u8')
+        rows = db_data['rows']
+        try:
+            for row in rows:
+                self.assertEqual('ben folds', row['artist_key'])
+                self.assertEqual('thealbum', row['album_key'])
+                self.assertEqual('31', row['track_key'])
+                self.assertEqual('track title', row['title_key'])
+        finally:
+            db_data['cursor'].close()
+            db_data['conn'].close()
+
+
+    def test_no_underscore_in_artist_name_but_in_song(self):
+        db_data = test_utils.insert_playlist_into_music_map('test_artist_has_underscore_but_not_song.m3u8')
+        rows = db_data['rows']
+        try:
+            for row in rows:
+                self.assertEqual('ben folds', row['artist_key'])
+                self.assertEqual('thealbum', row['album_key'])
+                self.assertEqual('31', row['track_key'])
+                self.assertEqual('track title', row['title_key'])
+        finally:
+            db_data['cursor'].close()
+            db_data['conn'].close()
+
+
+    def test_nonexistent_playlist(self):
+        self.assertRaises(SystemExit, test_utils.insert_playlist_into_music_map, 'nonexistent.m3u8')
+
+
+    def test_unparseable_playlist(self):
+        db_data = test_utils.insert_playlist_into_music_map('unparseable.m3u8')
+        try:
+            with open('unparseable.log', 'r') as log:
+                unparseable_song = log.readline().strip()
+                self.assertEqual('./Ben_Folds/TheAlbum/not track_Ben_Folds_The Track Title.mp3', unparseable_song)
+        finally:
+            db_data['cursor'].close()
+            db_data['conn'].close()
+
 if __name__ == "__main__":
     unittest.main()
