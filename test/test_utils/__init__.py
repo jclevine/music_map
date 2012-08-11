706b4cc4c8f2cfc3696286adcd87c96d273f1895
Justin Levine <justin.c.levine@gmail.com>
2012-08-11 15:31:25 -0400

    Fix test util so that it closes connections if there's an error.

--- test/test_utils/__init__.py
+++ test/test_utils/__init__.py
@@ -17,20 +17,25 @@ def insert_playlist_into_music_map(playlist_name,
               'music_roots': ['.'],
               'db_loc': db_loc,
               'debug': False}
-    MusicMap(params)
 
-    conn = sqlite3.connect(TEST_DB_LOC)
-    cursor = conn.cursor()
-    query = """
-            SELECT artist_key
-                 , album_key
-                 , track_key
-                 , title_key
-              FROM song
-            """
-    rs = cursor.execute(query)
-    return {'conn': conn,
-            'cursor': cursor,
-            'rows':sqlite_utils.name_columns(rs)}
+    try:
+        MusicMap(params)
 
+        conn = sqlite3.connect(TEST_DB_LOC)
+        cursor = conn.cursor()
+        query = """
+                SELECT artist_key
+                     , album_key
+                     , track_key
+                     , title_key
+                  FROM song
+                """
+        rs = cursor.execute(query)
+        return {'conn': conn,
+                'cursor': cursor,
+                'rows':sqlite_utils.name_columns(rs)}
+    except IOError as ioe:
+        conn.close()
+        cursor.close()
+        raise ioe
 
